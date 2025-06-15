11cbd448d424abe7adb4324aa5dd8ad7fa88d644
chenglu1129 <928720204@qq.com>
2025-06-15 19:04:54 +0800

    简化NullbrMcpServerApplication类，修正ToolCallbackProviderRegister类的包名，更新application.yml以移除web-application-type配置。

--- src/main/java/com/nullbr/mcp/NullbrMcpServerApplication.java
+++ src/main/java/com/nullbr/mcp/NullbrMcpServerApplication.java
@@ -1,14 +1,6 @@
 package com.nullbr.mcp;
-
-import com.nullbr.mcp.service.NullbrApiService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NullbrMcpServerApplication {
--- src/main/java/com/nullbr/mcp/config/ToolCallbackProviderRegister.java
+++ src/main/java/com/nullbr/mcp/config/ToolCallbackProviderRegister.java
@@ -1,6 +1,6 @@
-package com.cheng.mcpnullbrserversse.config;
+package com.nullbr.mcp.config;
 
-import com.cheng.mcpnullbrserversse.service.NullbrApiService;
+import com.nullbr.mcp.service.NullbrApiService;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
